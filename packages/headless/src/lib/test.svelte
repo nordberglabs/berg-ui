f79cfb30ded075fb6b2b4120180334d5636533f0
Liam <liam@lmjcbs.com>
2024-11-24 16:39:31 +0000

    [headless] Scaffold package

---
+++ packages/headless/src/lib/test.svelte
@@ -0,0 +1,15 @@
+<script>
+	// @ts-check
+
+	/**
+	 * Adds two numbers.
+	 * @param {number} a
+	 * @param {number} b
+	 * @returns {number}
+	 */
+	function add(a, b) {
+		return a + b;
+	}
+
+	const result = add(7, 6);
+</script>
